5a38d1bc4cbbf4d5d96414145448ac9e291428c5
manny-hub <ayomideadeyeyee1@gmail.com>
2026-02-15 13:21:32 +0100

    update

--- src/transforms/_batch_process.py
+++ src/transforms/_batch_process.py
@@ -1,10 +1,28 @@
 import json 
-from utils.commons import hash, ingest_at, read_json
+from utils.commons import hash, ingest_at, 
+from dotenv import load_dotenv
+import os 
+
+
+load_dotenv()
 
 
 paths ="data/bootstrap/*.json" 
 
+def get_config():
+    
+    MONGO_URL = os.getenv("MONGO_URI")
+    MONGO_DBNAME = os.getenv("MONGO_DBNAME")
+    MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
+
 def read_event(file_path):
     
     data = read_json(paths)
+    raw = {
+        "event_id": hash([data["event_name"], data["timestamp"]]),
+        "event_name": data["event_name"],
+        "timestamp": data["timestamp"],
+        "ingest_at": ingest_at(),
+    }
+    
     
