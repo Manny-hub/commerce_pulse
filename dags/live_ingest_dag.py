4e313f9477d2a20918b6243413c27bf8e3038527
manny-hub <ayomideadeyeyee1@gmail.com>
2026-02-20 04:14:16 +0100

    update on live ingest orchestratation

---
+++ dags/live_ingest_dag.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone
+from datetime import timedelta
+from utils import run_ingest
+
+default_args = {
+    "owner": "airflow",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="commerce_pulse_mongodb_ingest",
+    default_args=default_args,
+    description="Daily live ingest from CommercePulse Api to MongoDB",
+    schedule="@daily",
+    start_date=timezone.datetime(2026, 2, 20),
+    catchup=False,
+) as dag:
+
+    ingest_task = PythonOperator(
+        task_id="fetch_and_upsert_to_mongo",
+        python_callable=run_ingest,
+    )
